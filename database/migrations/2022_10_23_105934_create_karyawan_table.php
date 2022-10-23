de42a5aefd5381be480475e6a1a202e5ff4c6a5c
calvinadhikang <calvinadhikang@gmail.com>
2022-10-23 18:17:49 +0700

    creating database migration

---
+++ database/migrations/2022_10_23_105934_create_karyawan_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('karyawan', function (Blueprint $table) {
+            $table->id();
+            $table->string('username');
+            $table->string('password');
+            $table->string('nama');
+            $table->string('email');
+            $table->integer('telepon');
+            $table->integer('jenis_kelamin');
+            $table->integer('jabatan');
+            $table->integer('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('karyawan');
+    }
+};
---
+++ database/migrations/2022_10_23_111659_create_asal_barang_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('asal_barang', function (Blueprint $table) {
+            $table->integer('id_barang');
+            $table->integer('id_supplier');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('asal_barang');
+    }
+};
