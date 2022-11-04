f5e8a45263ff1ef49b97b7dc8acbc462e672eac8
calvinadhikang <calvinadhikang@gmail.com>
2022-11-04 14:03:16 +0700

    modelKaryawanFix

--- app/Models/Karyawan.php
+++ app/Models/Karyawan.php
@@ -9,7 +9,6 @@ class Karyawan extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD
     protected $table = "karyawan";
     protected $primaryKey = "id_karyawan";
     public $timestamps = false;
@@ -21,7 +20,4 @@ class Karyawan extends Model
         'jabatan',
         'jk'
     ];
-=======
-    protected $table = "Karyawan";
->>>>>>> 673161f2688f413f970d6413cd335ba320b56a3c
 }
