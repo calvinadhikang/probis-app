b1bf7dc4dbcbb44594ec6bc712f123f8bac322e8
calvinadhikang <calvinadhikang@gmail.com>
2022-11-04 14:12:23 +0700

    fixing

--- app/Http/Controllers/MasterKaryawanController.php
+++ app/Http/Controllers/MasterKaryawanController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Karyawan;
 use App\Rules\CustomRule;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class MasterKaryawanController extends Controller
 {
@@ -27,52 +26,8 @@ class MasterKaryawanController extends Controller
 
         ]);
     }
-    // berhasil
     public function ViewKaryawan()
     {
-        $karyawan = DB::table('karyawan')->get();
-        return view("master.karyawan.view", ["dataKaryawan" => $karyawan]);
-    }
-
-
-    public function GoAddKaryawan()
-    {
-        return view('master.karyawan.add');
-    }
-
-    public function addKaryawan(Request $request){
-        $in = $request->validate([
-            "username" => ["required"],
-            "nama" => ["required"],
-            "pass" => ["required"],
-            "conPass" => ["required", "same:pass"],
-            "notel" => ["required"],
-            "jabatan" => ["required"],
-            "jk" => ["required"]
-        ]);
-        $result = DB::insert('INSERT INTO karyawan(username_karyawan,nama_karyawan, password_karyawan, notel_karyawan, jabatan_karyawan, jk_karyawan) VALUES(?,?,?,?,?,?)', [
-            $in["username"],
-            $in["nama"],
-            $in["pass"],
-            $in["notel"],
-            $in["jabatan"],
-            $in["jk"]
-        ]);
-        if($result){
-            return redirect()->back()->with("success", "Berhasil Register!");
-        }else{
-            return redirect()->back()->with("error", "Gagal register!");
-        }
-        // $in = $request->validate([
-        //     "username" => ["required"],
-        //     "nama" => ["required"],
-        //     "password" => ["required"],
-        //     "conPass" => ["required", "same:pass"],
-        //     "notel" => ["required"],
-        //     "jabatan" => ["required"],
-        //     "jk" => ["required"]
-        // ]);
-        // Karyawan::create($request->all());
         $karyawans = Karyawan::all();
         return view('master.karyawan.view', [
             "karyawans" => $karyawans
